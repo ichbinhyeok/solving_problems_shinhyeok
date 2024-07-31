ad5a7bdbe72e183d8cdd9b563c3c35ad9b97ff56
shinhyeok22 <shinhyeok22@gmail.com>
2024-07-31 12:39:51 +0900

    04

--- src/Quiz.java
+++ src/Quiz.java
@@ -1,88 +1,62 @@
-
-
 import java.util.*;
 
 public class Quiz {
     Scanner sc = new Scanner(System.in);
 
-
     Map<Integer, Map<String, Object>> quizData = new HashMap<>();
     List<Integer> answerList = new ArrayList<>();
 
 
-    public void initAnswer() {
-
-        Map<String, Object> q1Data = new HashMap<>();
-        q1Data.put("answer", 2);
-        q1Data.put("score", 10);
-        quizData.put(1, q1Data);
-
-
-        Map<String, Object> q2Data = new HashMap<>();
-        q2Data.put("answer", 1);
-        q2Data.put("score", 15);
-        quizData.put(2, q2Data);
-
-
-        Map<String, Object> q3Data = new HashMap<>();
-        q3Data.put("answer", 1);
-        q3Data.put("score", 10);
-        quizData.put(3, q3Data);
-
-
-        Map<String, Object> q4Data = new HashMap<>();
-        q4Data.put("answer", 2);
-        q4Data.put("score", 5);
-        quizData.put(4, q4Data);
+    public void initQuiz() {
+        quizData.put(1, createQuestionData(2, 10, "1. Python에서 변수를 선언하는 방법은?",
+                new ArrayList<>(Arrays.asList("var name", "name = value", "set name", "name == value"))));
+        quizData.put(2, createQuestionData(1, 15, "2. Python에서 리스트(List)의 특징은 무엇인가요?",
+                new ArrayList<>(Arrays.asList("순서가 있고 변경 가능하다", "중복된 값을 가질 수 없다", "원소를 추가하거나 삭제할 수 없다", "정렬된 상태로 유지된다"))));
+        quizData.put(3, createQuestionData(1, 10, "3. Python에서 조건문을 작성하는 방법은?",
+                new ArrayList<>(Arrays.asList("if-else", "for-in", "while", "def"))));
+        quizData.put(4, createQuestionData(2, 5, "4. Python에서 함수를 정의하는 방법은?",
+                new ArrayList<>(Arrays.asList("class", "def", "import", "return"))));
     }
 
-    public void Q01() {
-        System.out.println("1. Python에서 변수를 선언하는 방법은? (점수: 10점)");
-        System.out.println("1) var name 2) name = value 3) set name 4) name == value");
+
+    private Map<String, Object> createQuestionData(int answer, int score, String question, List<String> options) {
+        Map<String, Object> data = new HashMap<>();
+        data.put("answer", answer);
+        data.put("score", score);
+        data.put("question", question);
+        data.put("options", options);
+        return data;
+    }
+
+
+    public void askQuestion(int questionNumber) {
+        Map<String, Object> questionData = quizData.get(questionNumber);
+        System.out.println(questionData.get("question") + " (점수: " + questionData.get("score") + "점)");
+        List<String> options = (List<String>) questionData.get("options");
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println((i + 1) + ") " + options.get(i));
+        }
         System.out.print("-정답 : ");
         int num = sc.nextInt();
         answerList.add(num);
     }
 
-    public void Q02() {
-        System.out.println("2. Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)");
-        System.out.println("1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 \n" +
-                "상태로 유지된다");
-        System.out.print("-정답 : ");
-        int num = sc.nextInt();
-        answerList.add(num);
-    }
-
-    public void Q03() {
-        System.out.println("3. Python에서 조건문을 작성하는 방법은? (점수: 10점)");
-        System.out.println("1) if-else, 2) for-in, 3) while, 4) def");
-        System.out.print("-정답 : ");
-        int num = sc.nextInt();
-        answerList.add(num);
-    }
-
-    public void Q04() {
-        System.out.println("4. Python에서 함수를 정의하는 방법은? (점수: 5점)");
-        System.out.println("1) class, 2) def, 3) import, 4) return");
-        System.out.print("-정답 : ");
-        int num = sc.nextInt();
-        answerList.add(num);
-    }
 
     public void result() {
         int totalScore = 0;
         System.out.println("----- 결과 -----");
         System.out.print("응답한 내용 : ");
-        totalScore = getTotalScore(totalScore);
+        totalScore = getTotalScore();
         System.out.println();
         System.out.println("당신 응답 합계: " + totalScore);
     }
 
-    private int getTotalScore(int totalScore) {
+
+    private int getTotalScore() {
+        int totalScore = 0;
         for (int i = 0; i < answerList.size(); i++) {
             int questionNumber = i + 1;
             int userAnswer = answerList.get(i);
-
             Map<String, Object> questionData = quizData.get(questionNumber);
             int correctAnswer = (int) questionData.get("answer");
             int score = (int) questionData.get("score");
@@ -91,18 +65,18 @@ public class Quiz {
             if (userAnswer == correctAnswer) {
                 totalScore += score;
             }
-
         }
         return totalScore;
     }
 
-    public void runQuiz() {
-        initAnswer();
-        Q01();
-        Q02();
-        Q03();
-        Q04();
-        result();
 
+    public void runQuiz() {
+        initQuiz();
+        for (int i = 1; i <= quizData.size(); i++) {
+            askQuestion(i);
+        }
+        result();
     }
+
+
 }
